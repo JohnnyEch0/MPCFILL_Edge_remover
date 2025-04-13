a23ce209ffdcc52bfcbd54d41dd44f5760313291
JohnnyEch0 <JohnS7y3f@gmail.com>
2025-04-13 17:40:54 +0200

    cut factor working

--- cmd_parse.py
+++ cmd_parse.py
@@ -10,9 +10,11 @@ Welcome = """
 def main():
 
     dimensions = (1490, 2080)
-    input_folder = os.path.dirname(sys.executable) #sys.path wont work for the executable
+    input_folder = os.path.dirname(sys.executable) 
+    # sys.path[0] (for coding) #os.path.dirname(sys.executable) <-- for .exe
     output_folder = "edge_remover_output"
     test_mode = False
+    crop_factor = 0.11
 
     print(Welcome)   
     
@@ -23,11 +25,12 @@ def main():
             images_folder: {input_folder}
             output_folder: {output_folder}
             test_mode: {test_mode}
+            crop_factor: {crop_factor}
         """
 
         print("Change Settings?")
         print(SETTINGS)
-        user_input = input("-d to change dimensions\n-i to change input folder\n-o to change output folder\n-q to quit \n-t to change test mode \n Enter to continue \n")
+        user_input = input("-d to change dimensions\n-i to change input folder\n-o to change output folder\n-q to quit \n-t to change test mode \n -c to change crop factor \n Enter to continue \n")
         if user_input == "-d":
 
             width = input("Enter target width: \n")
@@ -39,10 +42,12 @@ def main():
             output_folder = input("Enter output folder: ")
         elif user_input == "-t":
             test_mode = not test_mode
+        elif user_input == "-c":
+            crop_factor = float(input("Enter desired crop ratio (0.12 is default)"))
         elif user_input == "-q":
             sys.exit(0)
         else:
             break
 
     print(SETTINGS)
-    return input_folder, output_folder, dimensions[0], dimensions[1], test_mode
+    return input_folder, output_folder, dimensions[0], dimensions[1], test_mode, crop_factor
